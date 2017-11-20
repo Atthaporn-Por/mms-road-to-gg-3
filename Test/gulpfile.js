3a91ad523949ef884af3a4c677d5998418ed8347
Thanakit-G <thanakit_1995@hotmail.com>
2017-11-20 20:58:54 +0900

    Edit gulp

--- Test/gulpfile.js
+++ Test/gulpfile.js
@@ -3,9 +3,8 @@
 
 // grab our gulp packages
 var gulp  = require('gulp'),
-    gutil = require('gulp-util');
 
 // create a default task and just log a message
 gulp.task('default', function() {
-  return gutil.log('Gulp is running!')
+  console.log('Gulp is running!')
 });
