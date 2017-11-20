477024d02c2dfd1f94d5164869dc60eecc245bc3
Thanakit-G <thanakit_1995@hotmail.com>
2017-11-20 21:43:36 +0900

    Add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+/* File: gulpfile.js */
+
+// grab our gulp packages
+var gulp  = require('gulp'),
+
+// create a default task and just log a message
+gulp.task('default', function() {
+  console.log('Gulp is running!')
+});
