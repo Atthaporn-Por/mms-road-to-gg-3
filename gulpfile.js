90da67e734bd01ce4aa6bc5887adafae3d674aaf
Thanakit-G <thanakit_1995@hotmail.com>
2017-11-20 22:54:20 +0900

    Delete gulpfile.js

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-/* File: gulpfile.js */
-
-// grab our gulp packages
-var gulp  = require('gulp'),
-
-// create a default task and just log a message
-gulp.task('default', function() {
-  console.log('Gulp is running!')
-});
