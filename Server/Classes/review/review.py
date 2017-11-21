2fb8a3abec937e05e617f1b2141253bd166fbcad
5730175121-NR <nuttapol7185@gmail.com>
2017-11-21 09:19:48 +0700

    add review class

---
+++ Server/Classes/review/review.py
@@ -0,0 +1,6 @@
+class Review:
+
+    def __init__(self,transactionID='',comments='',ratting=''):
+        self.transactionID = transactionID
+        self.comments = comments
+        self.ratting = ratting
