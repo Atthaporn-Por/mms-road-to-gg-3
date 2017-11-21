a13edd82964a6aa72ac016b57968dbff37a1ea06
5730175121-NR <nuttapol7185@gmail.com>
2017-11-21 09:04:40 +0700

    add chatting system class

---
+++ Server/Classes/chatting/chatting_control.py
@@ -0,0 +1,13 @@
+class ChattingControl:
+
+    def send(message=''):
+        print('send message : %s' % message)
+
+    def read(messageID=0):
+        print('read message : %s' % str(messageID))
+
+    def save_chat():
+        print('save chat')
+
+    def ordering():
+        print('ordering')
