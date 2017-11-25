b36cab8271fdad30c095f9d589db13f00370af37
Thanakit-G <thanakit_1995@hotmail.com>
2017-11-25 21:49:53 +0700

    Add files via upload

---
+++ Test/test_login.py
@@ -0,0 +1,18 @@
+# test_login.py
+import pytest
+import Server/Classes/user.py
+
+def test_userInvalid
+    assert True
+
+def test_wrongPassword
+    assert True
+
+def test_oAuthTest
+    assert True
+    
+def test_callerDataTest
+    assert True
+
+def test_isInputValid
+    assert True
