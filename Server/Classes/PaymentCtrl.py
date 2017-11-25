dea591e5af05a208e52fc67c4b0b63b9dfdc579f
Tara Tammanutam <pouy1942@hotmail.com>
2017-11-25 23:22:49 +0700

    fill function getAmountfromTran

--- Server/Classes/PaymentCtrl.py
+++ Server/Classes/PaymentCtrl.py
@@ -6,11 +6,42 @@ class PaymentCtrl:
         print('payment')
 
     def sendPaymentinfo(self,transactionID):
-        print('sentPayment')
+        #print('sentPayment')
+        self.transactionID = transactionID
 
     def collectTransaction(self):
-        pirint('collectTransaction')
-        
+        print('collectTransaction')
 
-    def getAmountfromTran(self,transactionID):
-        print('getAmountfromTran')
+
+    def getAmountfromTran(self,distance):
+        fair = 35
+        dis = distance
+
+        while (dis >= 1):
+            if dis >= 80 :
+                x = dis - 80
+                fair += x*10.5
+                dis = dis - x
+            elif 60 <= dis < 80:
+                x = dis - 60
+                fair += x*9
+                dis = dis -60
+            elif 40 <= dis < 60 :
+                x = dis - 40
+                fair += x * 8
+                dis = dis - 40
+            elif 20 <= dis < 40 :
+                x = dis - 20
+                fair += x * 7.5
+                dis = dis - 20
+            elif 10 <= dis < 20 :
+                x = dis - 10
+                fair += x * 6.5
+                dis = dis - 10
+            elif 1 <= dis < 10 :
+                fair += dis * 5.5
+                dis = 0
+
+        return  fair
+
+
