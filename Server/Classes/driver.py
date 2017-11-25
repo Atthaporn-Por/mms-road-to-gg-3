71b95eeb16994005cf9fafe308f7d0015a121c57
5730175121-NR <nuttapol7185@gmail.com>
2017-11-25 21:37:44 +0700

    @function search Nearby User

--- Server/Classes/driver.py
+++ Server/Classes/driver.py
@@ -9,6 +9,29 @@ class Driver:
         self.drivePermissionID = drivePermissionID
         self.SSN = SSN
 
+    def searchUser(self, latitude , longitude):
+        mysql = MySQL()
+        min_latitude = latitude - 10.0
+        max_latitude = latitude + 10.0
+        min_longitude = longitude - 10.0
+        max_longitude = longitude + 10.0
+        list_of_results = mysql.query('SELECT * FROM gettaxi.subcriber_user WHERE ((real_time_lat_location BETWEEN %s AND %s) AND (real_time_long_location BETWEEN %s AND %s) AND (status = "calling"))', (min_latitude,max_latitude,min_longitude,max_longitude))
+        list_of_user = []
+        for result in list_of_results:
+            (phone, real_time_lat_location, real_time_long_location, status) = result
+            real_time_lat_location = float("{0:.6f}".format(real_time_lat_location))
+            real_time_long_location = float("{0:.6f}".format(real_time_long_location))
+            user_data = {
+                'phone' : phone,
+                'real_time_lat_location' : real_time_lat_location,
+                'real_time_long_location' : real_time_long_location,
+                'status' : status
+            }
+            list_of_user.append(user_data)
+        mysql.close()
+        return {'driver' : list_of_user }
+
+
     def accept(self, transactionID):
         # return int
     
